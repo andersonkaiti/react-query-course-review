1ebe7c458a286164410d58a2ee14277fc6dff8e9
Anderson Kaiti <anderkaiti@gmail.com>
2026-07-23 23:45:30 -0300

    :wrench: chore(commitlint): setup missing commitlint-config-gitmoji

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,7 @@
 import type { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['@commitlint/config-conventional', 'gitmoji'],
   rules: {
     'body-empty': [2, 'always'],
     'footer-empty': [2, 'always'],
