91419ec1af8ee0ef0a1ced78b434627c2c124429
Anderson Kaiti <anderkaiti@gmail.com>
2026-07-24 18:37:48 -0300

    :sparkles: feat(lifecycle): explore query lifecycle states and refetch

--- src/users.tsx
+++ src/users.tsx
@@ -7,16 +7,47 @@ interface IUser {
 }
 
 export function Users() {
-  const { data } = useQuery({
+  // const [signedIn, setSignedIn] = useState(false)
+
+  const {
+    data,
+    refetch,
+    isLoading,
+    isPending: _isPending,
+    isFetching,
+  } = useQuery({
+    // propriedade reativa
+    enabled: false,
     queryKey: ['users'],
     queryFn: async (): Promise<IUser[]> => {
+      await new Promise((resolve) => setTimeout(resolve, 1500))
       const response = await fetch('http://localhost:3000/users')
       return await response.json()
     },
   })
 
+  /**
+   * isLoading: é o loading inicial.
+   * isPending: é true enquanto não houver valor no cache.
+   * isFetching: é true sempre que a queryFn é executada.
+   *
+   * isLoading = isPending && isFetching
+   */
+
   return (
-    <div>
+    <div className="p-4">
+      <button
+        type="button"
+        className="cursor-pointer rounded-lg bg-white px-4 py-2 text-black hover:bg-zinc-300"
+        // onClick={() => setSignedIn(true)}
+        onClick={() => refetch()}
+      >
+        Listas usuários
+      </button>
+
+      {isLoading && <p>Carregando...</p>}
+      {!isLoading && isFetching && <small className="block">Fetching...</small>}
+
       {data?.map((user) => (
         <div key={user.id}>
           <strong className="block">{user.name}</strong>
