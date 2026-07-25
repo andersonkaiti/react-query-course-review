4ad1f710cf156a9bd579c57febb855fa2fd7ef88
Anderson Kaiti <anderkaiti@gmail.com>
2026-07-25 20:10:47 -0300

    :sparkles: feat(prefetch): prefetch users query on link hover

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,7 +1,24 @@
+import { useQueryClient } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
+import { getUsers } from '../http/get-users'
+
 export function Posts() {
+  const queryClient = useQueryClient()
+
+  function handleMouseEnter() {
+    queryClient.prefetchQuery({
+      queryKey: ['users'],
+      queryFn: getUsers,
+    })
+  }
+
   return (
-    <div className="p-5">
+    <div className="space-y-4 p-5">
       <h1>Posts</h1>
+
+      <Link to="/" className="underline" onMouseEnter={handleMouseEnter}>
+        Ir para os usuários
+      </Link>
     </div>
   )
 }
